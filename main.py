03c68bfc4a53ef26b5b4a1daf81a99ae8fd23e3d
Illia25 <steam8837@gmail.com>
2024-01-03 22:05:06 +0200

    added conditions task 2

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-# print("Hello World!")
-# print("Hello World!")
-# завдання 1
-# number1 = int(input("Введіть число: "))
-# number2 = int(input("Введіть число: "))
-# number3 = int(input("Введіть число: "))
-# Вибір операції від користувача
-# print("Оберіть операцію:")
-# print("1. Максимум")
-# print("2. Мінімум")
-# print("3. Середньоарифметичне")
-# choice = int(input("Ваш вибір (введіть число від 1 до 3): "))
-# Виконання вибраної операції
-# if choice == 1:
-#     result = max(number1, number2, number3)
-#     print("Максимум: ", result)
-# elif choice == 2:
-#     result = min(number1, number2, number3)
-#     print("Мінімум: ", result)
-# elif choice == 3:
-#     result = (number1 + number2 + number3) / 3
-#     print("Середньоарифметичне: ", result)
-# else:
-#     print("Введено невірний вибір операції. Будь ласка, виберіть число від 1 до 3.")
-
 # завдвння 2
 
 # Введення кількості метрів від користувача
